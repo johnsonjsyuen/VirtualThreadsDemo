b86077a35bc4a2ecce3fbb7305d3436fdb547a89
johnson yuen <j@johnsonyuen.com>
2024-10-01 20:32:05 +1300

    First, working

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,109 @@
+package org.johnson
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import org.apache.commons.lang3.concurrent.TimedSemaphore
+
+fun main() {
+    basicStartAThread()
+    virtualThreadExecutor()
+    fixedThreadPoolExecutor()
+    virtualThreadExecutorWithSemaphore()
+}
+
+fun basicStartAThread() {
+    val virtualThread = Thread.startVirtualThread {
+        println(
+            "Running task in a virtual thread: single"
+        )
+    }
+    // Waiting for virtual threads to complete
+    try {
+        virtualThread.join()
+    } catch (e: InterruptedException) {
+        e.printStackTrace()
+    }
+}
+
+fun virtualThreadExecutor(){
+    val executor: ExecutorService = Executors.newVirtualThreadPerTaskExecutor()
+    val client = HttpClient.newHttpClient()
+    for (i in 0..99) {
+        executor.submit {
+            println(
+                "Running task in a virtual thread: $i"
+            )
+            try {
+                val request = HttpRequest.newBuilder()
+                    .uri(URI("https://dummyjson.com/products/$i"))
+                    .GET()
+                    .build()
+
+                val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+                println("Response for request $i: ${response.body()}")
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+    executor.awaitTermination(10, java.util.concurrent.TimeUnit.SECONDS)
+    executor.shutdown()
+}
+
+fun fixedThreadPoolExecutor(){
+    val executor: ExecutorService = Executors.newFixedThreadPool(2)
+    val client = HttpClient.newHttpClient()
+    for (i in 0..99) {
+        executor.submit {
+            println(
+                "Running task in a virtual thread: $i"
+            )
+            try {
+                val request = HttpRequest.newBuilder()
+                    .uri(URI("https://dummyjson.com/products/$i"))
+                    .GET()
+                    .build()
+
+                val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+                println("Response for request $i: ${response.body()}")
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+    executor.awaitTermination(10, java.util.concurrent.TimeUnit.SECONDS)
+    executor.shutdown()
+}
+
+fun virtualThreadExecutorWithSemaphore(){
+    val executor: ExecutorService = Executors.newVirtualThreadPerTaskExecutor()
+    val client = HttpClient.newHttpClient()
+    val semaphore = TimedSemaphore(1, java.util.concurrent.TimeUnit.SECONDS, 2)
+    for (i in 0..99) {
+        executor.submit {
+            semaphore.acquire()
+            println(
+                "Running task in a virtual thread: $i"
+            )
+            try {
+
+                val request = HttpRequest.newBuilder()
+                    .uri(URI("https://dummyjson.com/products/$i"))
+                    .GET()
+                    .build()
+
+                val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+                println("Response for request $i: ${response.body()}")
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+    executor.awaitTermination(10, java.util.concurrent.TimeUnit.SECONDS)
+    executor.shutdown()
+}
